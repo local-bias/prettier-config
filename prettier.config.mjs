eb9a0b28c8e34ad72af940c382322d7be30a5d65
Ribbit <echo07200503@gmail.com>
2024-09-05 15:03:34 +0900

    🚀 initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type { import("prettier").Options } */
+export default {
+  printWidth: 100,
+  arrowParens: "always",
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  endOfLine: "lf",
+};
